d28c59e9930f20e46a4745b4668e5762f7d0afb5
Grzesiek <grzegorzkowalczyk1989@gmail.com>
2022-01-19 22:48:23 +0100

    mapped RestController methods

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,16 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-		TaskDto taskDto = new TaskDto(
-				(long)1,
-				"Test title",
-				"I want to be a coder"
+		//TaskDto taskDto = new TaskDto(
+			//	(long)1,
+				//"Test title",
+				//"I want to be a coder"
 
-		);
-		Long id = taskDto.getId();
-		String title = taskDto.getTitle();
-		String content = taskDto.getContent();
-		System.out.println(id + " " + title + " " + content);
+		//);
+		//Long id = taskDto.getId();
+		//String title = taskDto.getTitle();
+		//String content = taskDto.getContent();
+		//System.out.println(id + " " + title + " " + content);
 		SpringApplication.run(TasksApplication.class, args);
 	}
 }
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.crud.tasks.domain.TaskDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -11,25 +12,24 @@ import java.util.List;
 public class TaskController {
 
     //@RequestMapping(method = RequestMethod.GET,value = "") =  @GetMapping
-    @GetMapping
+    @GetMapping(value = "/getTask")
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
-    @GetMapping
-    TaskDto getTask (Long taskId) {
+    @GetMapping(value = "{taskId}")
+    TaskDto getTask (@PathVariable Long taskId) {
         return new TaskDto(1L,"test title","test_content");
     }
-    @DeleteMapping
-    public void deleteTask(Long taskId) {
+    @DeleteMapping(value = "{taskId}")
+    public void deleteTask(@PathVariable Long taskId) {
 
     }
     @PutMapping
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test_content");
     }
-    @PostMapping
+    @PostMapping(value = "/createTask")
     public void createTask(TaskDto taskDto) {
 
     }
-
 }
