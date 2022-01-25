ed5899ddf2b18cf204d5ee03749b2c989ebbcfa8
Grzesiek <grzegorzkowalczyk1989@gmail.com>
2022-01-25 10:19:22 +0100

    19.3 add deleteById method

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,6 +4,24 @@ import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*Aplikacja  "TASKS" -  odczyt  zadań z bazy danych o konkretnej encji ID
+
+1. Klasa DbServive odpowiada za komunikację z bazą danych "tasks_crud".
+Klasa ta wstrzykuje do siebie klasę TaskRepository (zawiera interfejs rozrzerzony o
+CrudRepository - znajdujący się w bibliotekach sprongframework)
+
+ przy pomocy adnotacji @Autowired
+2. Klasa TaskMapper mapuje typ Task na TaskDto oraz odwrotnie
+
+3. W klasie TaskController dokonujemy wstrzyknięcia klas DbService oraz TaskMapper
+
+4. Przy użyciu Postman-a wysyłyamy żądania HTTP do controllera aplikacji, używamy metod :
+ - GET ( POBIERANIE DANYCH Z SERWERA)
+ - POST (WSTAWIANIE DANYCH NA SERWER)
+ - DELETE (USUWANIE DANYCH Z SERWERA)
+ - PUT (UPDATE DANYCH NA SERWERZE)
+ */
 @SpringBootApplication
 public class TasksApplication {
 
