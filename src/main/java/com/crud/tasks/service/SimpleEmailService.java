5dc04d0412ee7ffc3aa50c2ddceaaa3b2089b4fc
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-02-15 20:50:27 +0100

    adding 23.3

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -38,7 +38,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMail());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-       if (mail.getToCc() != "") {
+       if (mail.getToCc() != null && !mail.getToCc().isEmpty()) {
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
