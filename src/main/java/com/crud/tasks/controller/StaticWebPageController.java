4549477f51e32601f5a6ecedd02626c9367f5cf9
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-05-08 13:56:37 +0200

    32.1Zadanie: operacje matematyczne w Thymeleaf

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -9,15 +9,9 @@ import java.util.Map;
 public class StaticWebPageController {
 
     @RequestMapping("/")
-    public String index(Map<String,Object> model) {
+    public String index(Map<String, Object> model) {
         model.put("variable", "My thymeleaf variable");
-        model.put("one", 1);
         model.put("two", 2);
-        model.put("2", "two");
-        model.put("4", "four");
-        model.put("equal", "=");
-        model.put("multi", "*");
-        model.put("plus", "+");
         return "index";
     }
 }
