69fd77b8be237a3d20412490f4ca2eeb2d1f537f
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2022-05-09 06:30:44 +0200

    32.1Zadanie: operacje matematyczne w Thymeleaf

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -4,9 +4,12 @@ import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -20,27 +23,50 @@ import java.util.Optional;
 public class SimpleEmailService {
 
     private final JavaMailSender javaMailSender;
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     public void send(final Mail mail) {
         log.info("Starting email preparation....");
 
         try {
-            SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            // SimpleMailMessage mailMessage = createMailMessage(mail);
+            // javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
             log.info("Email has been sent");
         } catch (MailException e) {
             log.error("Failed to process email sending: " + e.getMessage(), e);
         }
     }
 
+    //private SimpleMailMessage createMailMessage(final Mail mail) {
+    //   SimpleMailMessage mailMessage = new SimpleMailMessage();
+    // mailMessage.setTo(mail.getMail());
+    // mailMessage.setSubject(mail.getSubject());
+    //mailMessage.setText(mail.getMessage());
+    // if (mail.getToCc() != null && !mail.getToCc().isEmpty()) {
+    //     mailMessage.setCc(mail.getToCc());
+    // }
+    // return mailMessage;
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMail());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+
+    }
+
     private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMail());
         mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-       if (mail.getToCc() != null && !mail.getToCc().isEmpty()) {
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        if (mail.getToCc() != null && !mail.getToCc().isEmpty()) {
             mailMessage.setCc(mail.getToCc());
         }
-        return mailMessage;
+            return mailMessage;
+        }
     }
-}
+
